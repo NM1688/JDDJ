7db41bbe6286cc0a04c61b1bdba8e4f4539254c0
Hydrahail-Johnson <zjx123126@gmail.com>
2021-03-05 17:05:01 +0800

    添加新脚本

--- i-chenzhe_jd_entertainment.js
+++ i-chenzhe_jd_entertainment.js
@@ -2,7 +2,7 @@
 百变大咖秀
 活动入口：首页搜索-‘百变大咖秀’-底部最右侧按钮
 请手动进入一次活动页面已确保能够自动抽奖
-活动地址：https://lzdz-isv.isvjcloud.com/dingzhi/change/able/activity/3508994?activityId=dz2102100001340204
+活动地址：https://lzdz-isv.isvjcloud.com/dingzhi/change/able/activity/3508994?activityId=dz2102100001340205
 
 新手写脚本，难免有bug，能用且用。
 多谢 whyour 大佬 指导
