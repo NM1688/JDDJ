a4397f8e0fb4396dd46721215650f1f033535f0d
qiaoqi <45731546+qiao112@users.noreply.github.com>
2021-03-09 23:13:16 +0800

    修改填写格式为一行一个

--- diy.sh
+++ diy.sh
@@ -1,46 +1,116 @@
 #!/usr/bin/env bash
 
+
 # 把此diy.sh放入config即可,会自动同步最新脚本
 # 如有好用的脚本或者脚本更新不及时请@qiao112
-# 2021年3月9日19:50
+# 2021年3月9日23:12
 
-############################## 作者昵称 ##############################
-author_list="Tartarus2014 i-chenzhe whyour moposmall qq34347476 ZCY01 Hydrahail shuye72"
 
-############################## 脚本地址 ##############################
+##############################################################################
+#                                                                            #
+#                          自动拉取各个作者库内指定脚本
+#                                                                            #
+##############################################################################
+
+
+############################## 作者名称 ##############################
+author_list="
+Tartarus2014
+i-chenzhe
+whyour
+moposmall
+qq34347476
+ZCY01
+Hydrahail
+shuye72
+
+"
+######################################################################
+
+
+############################## 维护:Tartarus2014 ##############################
+# 库地址:https://github.com/Tartarus2014/Script
 scripts_base_url_1=https://ghproxy.com/https://raw.githubusercontent.com/Tartarus2014/Script/master/
+my_scripts_list_1="
+
+"
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+############################## 维护:i-chenzhe ##############################
+# 库地址:https://github.com/i-chenzhe/qx
 scripts_base_url_2=https://ghproxy.com/https://raw.githubusercontent.com/i-chenzhe/qx/main/
+my_scripts_list_2="
+jd_entertainment.js
+jd_fanslove.js
+jd_gjmh.js
+jd_shake.js
+jd_shakeBean.js
+z_marketLottery.js
+z_superDay.js
+
+"
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+############################## 维护:whyour ##############################
+# 库地址:https://github.com/whyour/hundun/tree/master/quanx
 scripts_base_url_3=https://ghproxy.com/https://raw.githubusercontent.com/whyour/hundun/master/quanx/
+my_scripts_list_3="
+jd_zjd_tuan.js
+
+"
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+############################## 维护:moposmall ##############################
+# 库地址:https://github.com/moposmall/Script/tree/main/Me
 scripts_base_url_4=https://ghproxy.com/https://raw.githubusercontent.com/moposmall/Script/main/Me/
+my_scripts_list_4="
+jx_cfd_exchange.js
+
+"
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+############################## 维护:qq34347476 ##############################
+# 库地址:https://github.com/qq34347476/js_script
 scripts_base_url_5=https://ghproxy.com/https://raw.githubusercontent.com/qq34347476/js_script/master/scripts/
+my_scripts_list_5="
+format_share_jd_code.js
+
+"
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+############################## 维护:ZCY01 ##############################
+# 库地址:https://github.com/ZCY01/daily_scripts/tree/main/jd
 scripts_base_url_6=https://ghproxy.com/https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/
+my_scripts_list_6="
+jd_priceProtect.js
+
+"
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+############################## 维护:Hydrahail ##############################
+# 库地址:https://github.com/Hydrahail-Johnson/diy_scripts
 scripts_base_url_7=https://ghproxy.com/https://raw.githubusercontent.com/Hydrahail-Johnson/diy_scripts/main/
+my_scripts_list_7="
+shopsign.js
+
+"
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+############################## 维护:shuye72 ##############################
+# 库地址:https://gitee.com/shuye72/MyActions/tree/main ps:尽量不要使用此人脚本,
 scripts_base_url_8=https://gitee.com/shuye72/MyActions/raw/main/
+my_scripts_list_8="
 
-############################## 脚本名称 ##############################
-# 维护:Tartarus2014 库地址:https://github.com/Tartarus2014/Script
-my_scripts_list_1=""
+"
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-# 维护:i-chenzhe   库地址:https://github.com/i-chenzhe/qx
-my_scripts_list_2="jd_entertainment.js jd_fanslove.js jd_gjmh.js jd_mlyjy.js jd_shake.js jd_shakeBean.js jd_jump_jump.js jd_xmf.js z_marketLottery.js z_superDay.js"
-
-# 维护:whyour      库地址:https://github.com/whyour/hundun/tree/master/quanx
-my_scripts_list_3="jd_zjd_tuan.js"
-
-# 维护:moposmall   库地址:https://github.com/moposmall/Script/tree/main/Me
-my_scripts_list_4="jx_cfd_exchange.js"
-
-# 维护:qq34347476  库地址:https://github.com/qq34347476/js_script
-my_scripts_list_5="format_share_jd_code.js"
-
-# 维护:ZCY01       库地址:https://github.com/ZCY01/daily_scripts/tree/main/jd
-my_scripts_list_6="jd_priceProtect.js"
-
-# 维护:Hydrahail   库地址:https://github.com/Hydrahail-Johnson/diy_scripts
-my_scripts_list_7="shopsign.js"
-
-# 维护:shuye72     库地址:https://gitee.com/shuye72/MyActions/tree/main ps:尽量不要使用此人脚本,
-my_scripts_list_8=""
 
 ############################## 随机函数 ##############################
 rand(){
@@ -49,6 +119,7 @@ rand(){
     num=$(cat /proc/sys/kernel/random/uuid | cksum | awk -F ' ' '{print $1}')
     echo $(($num%$max+$min))
 }
+
 
 ############################## 下载脚本 ##############################
 cd $ScriptsDir
@@ -101,6 +172,7 @@ do
   index=$[$index+1]
 done
 
+
 ############################## 修改更新频率 ##############################
 echo -e "开始修改更新频率"
 if [ -f ${ListCron} ]; then
@@ -112,8 +184,10 @@ else
   echo -e "修改更新时间失败..."
 fi
 
+
 ############################## 更新群助力脚本 ##############################
 bash ${ConfigDir}/sharecode.sh
+
 
 ############################## 更新diy.sh ##############################
 cd $ConfigDir
