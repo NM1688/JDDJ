e2905908c0fe5bd55f0923178270f800dea3d7cf
Misaka09982 <41320755+Misaka09982@users.noreply.github.com>
2021-03-12 08:25:05 +0800

    Update diy.sh

--- sh/diy.sh
+++ sh/diy.sh
@@ -28,7 +28,7 @@ scripts_base_url_7=https://gitee.com/shuye72/MyActions/raw/main/
 my_scripts_list_1=""
 
 # 维护:i-chenzhe   库地址:https://github.com/i-chenzhe/qx
-my_scripts_list_2="jd_entertainment.js jd_fanslove.js jd_shake.js jd_shakeBean.js jd_gjmh.js z_marketLottery.js z_superDay.js z_unionPoster.js"
+my_scripts_list_2="jd_entertainment.js jd_fanslove.js jd_shake.js jd_shakeBean.js jd_gjmh.js z_marketLottery.js z_superDay.js z_unionPoster.js jd_xmf.js"
 
 # 维护:whyour      库地址:https://github.com/whyour/hundun/tree/master/quanx
 my_scripts_list_3="jd_zjd_tuan.js"
