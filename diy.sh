6a18df5f876b4cc73066d34b9dcc215e286307eb
HydraHail <8326343+Hydrahail-Johnson@users.noreply.github.com>
2021-03-30 23:26:07 +0800

    Update diy.sh
    添加京东试用脚本

--- diy.sh
+++ diy.sh
@@ -72,6 +72,7 @@ jx_cfd_exchange.js
 scripts_base_url_5=https://ghproxy.com/https://raw.githubusercontent.com/qq34347476/js_script/master/scripts/
 my_scripts_list_5="
 format_share_jd_code.js
+jd_try.js
 
 "
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
