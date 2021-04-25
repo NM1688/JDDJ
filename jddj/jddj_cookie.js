10636c44efac86ef801efde4012ce251e377d0b5
简单就好 <jiandjh@qq.com>
2021-04-25 22:22:47 +0800

    update

---
+++ jddj/jddj_cookie.js
@@ -0,0 +1,6 @@
+let cookies = [
+'deviceid_pdj_jd=H5_DEV_BE5CB076-AD36-49D1-BE23-D2B2D5AAFFFA;o2o_m_h5_sid=bc58a559-fe52-4d02-b030-a3b46294e36c',
+'deviceid_pdj_jd=H5_DEV_F5DCDC85-C811-4D9D-8B93-CAA45C2B62BE;o2o_m_h5_sid=d16f8f00-4278-443b-9a07-9f7030c6a96b',
+'deviceid_pdj_jd=H5_DEV_09BDC2C6-D614-4BF9-88DB-AD367F64F93D;o2o_m_h5_sid=cd99cd19-1c83-40e3-acba-0263f1f74ee9'
+];
+module.exports = cookies;
